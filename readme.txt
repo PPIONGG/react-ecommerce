a22526add0486881843b6ea78cf93da221c39ce8
Loga Thrim <114689788+PPIONGG@users.noreply.github.com>
2024-09-14 23:33:11 +0700

    first commit

---
+++ readme.txt
@@ -0,0 +1,10 @@
+Client
+1. ติดตั้ง react ชื่อโฟลเดอร์ client
+2. npm i react-router-dom
+
+------------------------------------------------------------
+Server
+1. npm init
+2. npm i bcryptjs cloudinary cookie-parser cors dotenv express jsonwebtoken mongoose multer nodemon
+3. สร้างไฟล์ gitignore
+4. สร้างไฟล์ server.js
---
+++ server/server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+mongoose
+  .connect(
+    "mongodb+srv://tammasorn3173:tammasorn3173@cluster0.3sodl.mongodb.net/"
+  )
+  .then(() => console.log("connected to db"))
+  .catch((error) => console.log(error));
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    alloweredHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Cache-Control",
+      "Expires",
+      "Pragma",
+    ],
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
